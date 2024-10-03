ddb36dc0cee09b2aeeaee23c6817a5b0c7cfcc22
cvazquezfgc <152400955+cvazquezfgc@users.noreply.github.com>
2024-10-03 18:24:06 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,18 @@
+// Función para cargar los datos desde una URL proporcionada
+async function loadData(url) {
+    try {
+        const response = await fetch(url);
+        if (!response.ok) {
+            throw new Error(`Error HTTP! Estado: ${response.status}`);
+        }
+        const data = await response.json();
+        return data;
+    } catch (error) {
+        console.error(`Error cargando datos de ${url}:`, error);
+        return null;
+    }
+}
+
 // Función para dibujar gráficos concatenados para LINIA COMPLETA
 async function drawFullLinePlot(trams, resumData) {
     document.getElementById('plot').innerHTML = '<h2>Espai-temps previsió rehabilitació de la línia completa</h2>';
@@ -121,250 +136,6 @@ async function drawSinglePlot(tram, resumData) {
     document.getElementById('plot').appendChild(infoContainer);
 }
 
-// Función para dibujar un gráfico específico
-async function drawPlot(tram, resumData, estacionsData, containerId = 'plot', addHorizontalLabels = false, pkMinGlobal = null, pkMaxGlobal = null, plotHeight = 500) {
-    let traces = [];
-    let stationAnnotations = [];
-    let shapes = [];
-
-    let pkMin = Infinity;
-    let pkMax = -Infinity;
-
-    function groupConsecutiveSegments(data) {
-        const groupedData = [];
-        let currentGroup = null;
-
-        data.forEach(segment => {
-            const pkInici = parseFloat(segment['PK inici']);
-            const pkFinal = parseFloat(segment['PK final']);
-            const previsio = segment['PREVISIÓ REHABILITACIÓ'];
-
-            if (currentGroup && currentGroup.PKFinal === pkInici && currentGroup.PREVISIO === previsio && currentGroup.via === segment.Via) {
-                currentGroup.PKFinal = pkFinal;
-                currentGroup.length += (pkFinal - pkInici) * 1000;
-            } else {
-                if (currentGroup) {
-                    groupedData.push(currentGroup);
-                }
-                currentGroup = {
-                    PKInici: pkInici,
-                    PKFinal: pkFinal,
-                    PREVISIO: previsio,
-                    length: (pkFinal - pkInici) * 1000,
-                    via: segment.Via
-                };
-            }
-        });
-
-        if (currentGroup) {
-            groupedData.push(currentGroup);
-        }
-
-        return groupedData;
-    }
-
-    const via1Data = resumData.filter(d => parseInt(d.Via) === 1 && d.TRAM === tram);
-    const via2Data = resumData.filter(d => parseInt(d.Via) === 2 && d.TRAM === tram);
-
-    const via1 = groupConsecutiveSegments(via1Data);
-    const via2 = groupConsecutiveSegments(via2Data);
-
-    if (via1.length > 0 || via2.length > 0) {
-        pkMin = Math.min(...via1.concat(via2).map(d => d.PKInici));
-        pkMax = Math.max(...via1.concat(via2).map(d => d.PKFinal));
-
-        if (pkMinGlobal !== null) pkMin = pkMinGlobal;
-        if (pkMaxGlobal !== null) pkMax = pkMaxGlobal;
-
-                traces.push({
-            x: via1.map(d => d.PREVISIO),
-            y: via1.map(d => d.PKFinal - d.PKInici),
-            base: via1.map(d => d.PKInici),
-            type: 'bar',
-            name: 'Vía 1',
-            orientation: 'v',
-            width: 0.5,
-            offset: 0,
-            marker: {
-                color: 'rgba(31, 119, 180, 1)'
-            },
-            hoverinfo: 'text',
-            hovertext: via1.map(d => `${Math.round(d.length)} m`),
-            hoverlabel: {
-                bgcolor: 'rgba(31, 119, 180, 1)',
-                font: {
-                    color: 'white'
-                }
-            }
-        });
-
-        traces.push({
-            x: via2.map(d => d.PREVISIO),
-            y: via2.map(d => d.PKFinal - d.PKInici),
-            base: via2.map(d => d.PKInici),
-            type: 'bar',
-            name: 'Vía 2',
-            orientation: 'v',
-            width: 0.5,
-            offset: 0.5,
-            marker: {
-                color: 'rgba(255, 127, 14, 1)'
-            },
-            hoverinfo: 'text',
-            hovertext: via2.map(d => `${Math.round(d.length)} m`),
-            hoverlabel: {
-                bgcolor: 'rgba(255, 127, 14, 1)',
-                font: {
-                    color: 'white'
-                }
-            }
-        });
-
-        // Añadir anotaciones y líneas de referencia para las estaciones
-        const estaciones = estacionsData.filter(d => d.Tram === tram);
-
-        stationAnnotations.push(...estaciones.map(d => ({
-            x: 2069,
-            y: parseFloat(d['PK']),
-            text: `<b>${d['Abreviatura']}</b>`,
-            showarrow: false,
-            font: {
-                color: 'black',
-                size: 14,
-                family: 'Arial, sans-serif'
-            },
-            xanchor: 'left',
-            yanchor: 'middle',
-            bgcolor: 'white',
-            bordercolor: 'gray',
-            borderwidth: 2,
-            borderpad: 5,
-            opacity: 1
-        })));
-
-        shapes.push(...estaciones.map(d => ({
-            type: 'line',
-            x0: 1995,
-            x1: 2069,
-            y0: parseFloat(d['PK']),
-            y1: parseFloat(d['PK']),
-            line: {
-                color: 'darkgray',
-                width: 1.5,
-                layer: 'below'
-            }
-        })));
-
-        // Añadir líneas y sombreado para los años y la línea roja para 2025
-        shapes = shapes.concat(addLinesAndShading(pkMin, pkMax));
-    }
-
-    // Configuración del layout del gráfico
-    const layout = {
-        title: addHorizontalLabels ? '' : `Espai-temps previsió rehabilitació del tram ${tram}`,
-        xaxis: {
-            title: addHorizontalLabels ? 'Any previsió rehabilitació' : '',
-            range: [1995, 2070],
-            tickvals: Array.from({ length: 75 }, (_, i) => 1995 + i).filter(year => year % 5 === 0),
-            tickangle: addHorizontalLabels ? -45 : 0,
-            showticklabels: addHorizontalLabels
-        },
-        yaxis: {
-            title: 'PK',
-            autorange: 'reversed',
-            range: [pkMax, pkMin],
-            tickvals: Array.from({ length: Math.ceil(pkMax - pkMin + 1) }, (_, i) => Math.floor(pkMin) + i),
-            ticktext: Array.from({ length: Math.ceil(pkMax - pkMin + 1) }, (_, i) => `${Math.floor(pkMin) + i}+000`)
-        },
-        showlegend: true,
-        legend: {
-            orientation: 'v',
-            x: 1.05,
-            xanchor: 'left',
-            y: 1
-        },
-        annotations: stationAnnotations,
-        shapes: shapes,
-        hovermode: 'closest',
-        margin: {
-            l: 150,
-            r: 150,
-            t: addHorizontalLabels ? 80 : 20, // Ajustar margen superior
-            b: addHorizontalLabels ? 50 : 20  // Ajustar margen inferior
-        },
-        height: plotHeight // Ajustar la altura del gráfico
-    };
-
-    // Dibujar la gráfica
-    Plotly.newPlot(containerId, traces, layout);
-}
-
-// Función para añadir líneas y sombreado
-function addLinesAndShading(pkMin, pkMax) {
-    let shapes = [];
-    for (let year = 1995; year <= 2069; year++) {
-        // Añadir líneas verticales para cada año
-        shapes.push({
-            type: 'line',
-            x0: year,
-            x1: year,
-            y0: pkMin,
-            y1: pkMax,
-            line: {
-                color: 'lightgray',
-                width: 0.8,
-                layer: 'below'
-            }
-        });
-
-        // Añadir sombreado cada 5 años
-        if (year % 5 === 0) {
-            shapes.push({
-                type: 'rect',
-                x0: year,
-                x1: year + 1,
-                y0: pkMin,
-                y1: pkMax,
-                fillcolor: 'rgba(211, 211, 211, 0.3)',
-                layer: 'below',
-                line: {
-                    width: 0
-                }
-            });
-        }
-    }
-
-    // Añadir sombreado rojo antes de 2025
-    shapes.push({
-        type: 'rect',
-        x0: 1995,
-        x1: 2025,
-        y0: pkMin,
-        y1: pkMax,
-        fillcolor: 'rgba(255, 0, 0, 0.1)',
-        layer: 'below',
-        line: {
-            width: 0
-        }
-    });
-
-    // Añadir línea roja en 2025
-    shapes.push({
-        type: 'line',
-        x0: 2025,
-        x1: 2025,
-        y0: pkMin,
-        y1: pkMax,
-        line: {
-            color: 'red',
-            width: 2,
-            layer: 'above'
-        }
-    });
-
-    return shapes;
-}
-
 // Inicializar la página y los eventos
 async function init() {
     const resumUrl = 'https://raw.githubusercontent.com/cvazquezfgc/planificacio-renovacio-via/main/resum.json';
@@ -424,4 +195,3 @@ function selectTramButton(button) {
 document.addEventListener('DOMContentLoaded', () => {
     init();
 });
-
